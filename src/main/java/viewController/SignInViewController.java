a24c6704999d3af58068583515e4dafa312ee5c7
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-12-06 21:30:59 +0100

    Event handling code has been added for the buttons and the labels that can be clicked. The viewing icon for th password changes and the password can shift between visible and hidden. For now images and icon from the assets are accesssed directly but in the future a persistence through the controller will be implemented. The actions of clicking the interactive lables and the enter button still need to vi develop to switch to the corresponding view.

--- src/main/java/viewController/SignInViewController.java
+++ src/main/java/viewController/SignInViewController.java
@@ -1,26 +1,38 @@
 package viewController;
 
 import controller.SignInController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class SignInViewController implements Initializable {
 
     private SignInController controller;
+    private boolean passwordHiddenState;
 
-    //TextFields
+    //TextFields and PasswordFields
     @FXML
     private TextField usernameTextField;
 
     @FXML
     private TextField passwordTextField;
+
+    @FXML
+    private PasswordField hiddenPasswordField;
 
     //ImageViews
     @FXML
@@ -28,6 +40,9 @@ public class SignInViewController implements Initializable {
 
     @FXML
     private ImageView topLogoImageView;
+
+    @FXML
+    private ImageView viewPasswordImageView;
 
     //Buttons
     @FXML
@@ -48,11 +63,141 @@ public class SignInViewController implements Initializable {
 
 
     public SignInViewController() {
+
         controller = new SignInController();
+        passwordHiddenState = true;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        //SETTING LAYOUT DETAILS
+        //TODO: set assest using persistence through the controller
+
+        //Setting the logo image at the top
+        try {
+            Image colouredLogoImage = new Image(Objects.requireNonNull(getClass().getResource("/assets/images/colouredLogo.png")).toURI().toString()); //// MOSTRABA UNA ADVERTENCIA Y LA SOLUCIÓN FUE ESE object.requireNonNUL
+            topLogoImageView.setImage(colouredLogoImage);
+        } catch (URISyntaxException | NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("LogoImage could not be found");
+        }
+
+        //Setting the closed eye image
+        setEyeIconOnViewPasswordButton();
+
+        //SETTING ON ACTION
+
+        //Enter Button
+        enterButton.setOnAction(enterWithUsernameAndPassword());
+        //View Password Button
+        viewPasswordButton.setOnAction(changeViewPasswordState());
+        //LabelsCLicked
+        forgotPasswordLabel.setOnMouseClicked(forgotPasswordEvent());
+        createNewAccountLabel.setOnMouseClicked(createNewAccountEvent());
+        browseProductsWithoutAccountLabel.setOnMouseClicked(browseWithoutAccountEvent());
+
     }
+
+
+
+    //Enter button event handler
+    private EventHandler<ActionEvent> enterWithUsernameAndPassword() {
+
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                replicateContentInTexFieldAndPasswordField();
+                //TODO: compare the username with the database and compare the password with the database through the controller's persistence
+                System.out.println("Username: " + usernameTextField.getText());
+                System.out.println("Password: " + passwordTextField.getText());
+            }
+        };
+
+    }
+
+    //Password related events
+
+    //View Password Button event handler
+    private EventHandler<ActionEvent> changeViewPasswordState() {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                //Replicating content of password field and password text field
+                replicateContentInTexFieldAndPasswordField();
+                //Change passwordHiddenState
+                passwordHiddenState = !passwordHiddenState;
+                //Make the overlapping passwordField not visible
+                hiddenPasswordField.setVisible(passwordHiddenState);
+                //Change Icon
+                //TODO: handle this icon change using the controller's persistence
+                setEyeIconOnViewPasswordButton();
+            }
+        };
+    }
+
+
+    private void replicateContentInTexFieldAndPasswordField(){
+        if(passwordHiddenState){
+            passwordTextField.setText(hiddenPasswordField.getText());
+        } else {
+            hiddenPasswordField.setText(passwordTextField.getText());
+        }
+    }
+
+    private void setEyeIconOnViewPasswordButton(){
+        //TODO: handle this icon change using the controller's persistence
+        try {
+            Image eyeImage;
+        if(passwordHiddenState){
+            eyeImage = new Image(Objects.requireNonNull(getClass().getResource("/assets/icons/eye/closeEye.png")).toURI().toString()); //// MOSTRABA UNA ADVERTENCIA Y LA SOLUCIÓN FUE ESE object.requireNonNUL
+        } else {
+            eyeImage = new Image(Objects.requireNonNull(getClass().getResource("/assets/icons/eye/openEye.png")).toURI().toString()); //// MOSTRABA UNA ADVERTENCIA Y LA SOLUCIÓN FUE ESE object.requireNonNUL
+        }
+
+        viewPasswordImageView.setImage(eyeImage);
+        } catch (URISyntaxException | NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("icon eye could not be found");
+        }
+
+    }
+
+    //Label events
+
+    private EventHandler<? super MouseEvent> browseWithoutAccountEvent() {
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("Browse without Account CLicked.");
+            }
+        };
+    }
+
+    private EventHandler<? super MouseEvent> createNewAccountEvent() {
+
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+                System.out.println("Create new Account CLicked.");
+
+            }
+        };
+
+    }
+
+    private EventHandler<? super MouseEvent> forgotPasswordEvent() {
+
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+                System.out.println("Forgot password CLicked.");
+
+            }
+        };
+    }
+
+
 }
