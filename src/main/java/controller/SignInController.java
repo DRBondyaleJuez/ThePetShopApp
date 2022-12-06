8de5e17499acf36818fefbddd31f8dca2ff08baa
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-12-06 17:05:59 +0100

    Preliminary classes controller and viewController for the signInview. The classes have only been started to develop.

--- src/main/java/controller/SignInController.java
+++ src/main/java/controller/SignInController.java
@@ -1,4 +1,6 @@
 package controller;
 
 public class SignInController {
+
+    //PERSISTENCE perhaps assetManager and databaseManager
 }
--- src/main/java/viewController/SignInViewController.java
+++ src/main/java/viewController/SignInViewController.java
@@ -1,4 +1,58 @@
 package viewController;
 
-public class SignInViewController {
+import controller.SignInController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignInViewController implements Initializable {
+
+    private SignInController controller;
+
+    //TextFields
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    //ImageViews
+    @FXML
+    private ImageView backgroundImageView;
+
+    @FXML
+    private ImageView topLogoImageView;
+
+    //Buttons
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Button viewPasswordButton;
+
+    //Labels
+    @FXML
+    private Label forgotPasswordLabel;
+
+    @FXML
+    private Label createNewAccountLabel;
+
+    @FXML
+    private Label browseProductsWithoutAccountLabel;
+
+
+    public SignInViewController() {
+        controller = new SignInController();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
