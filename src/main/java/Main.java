135844ef2feb0d46e6525eb6e9af1e3aa6a5fd74
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-12-06 15:17:49 +0100

    The main class only launches the ThePetShopAppLauncher Class 'Application.launch(ThePetShopAppLauncher.class, args);'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import core.ThePetShopAppLauncher;
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(ThePetShopAppLauncher.class, args);
     }
 }
