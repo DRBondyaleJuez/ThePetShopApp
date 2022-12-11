46f7fe23dbb0accc0ed84cd0e152744ca0e43318
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-12-11 15:02:30 +0100

    Minor changes. No importance.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,16 @@ import core.ThePetShopAppLauncher;
 import javafx.application.Application;
 
 public class Main {
+
     public static void main(String[] args) {
         Application.launch(ThePetShopAppLauncher.class, args);
     }
+
+
+
+
+
+
+
+
 }
