07f310b910f0bae3d88c1889215d70a2eb78e7f5
Daniel RBJ <d.r.bond.juez@hotmail.com>
2023-01-01 22:17:27 +0100

    Minor change. Certain enums did not have a string associated. For example the last login enum so the table did not update during login. this has been fixed.

--- src/main/java/persistence/database/dbConnection/dbTablesEnums/UsersTableColumnNameEnums.java
+++ src/main/java/persistence/database/dbConnection/dbTablesEnums/UsersTableColumnNameEnums.java
@@ -3,7 +3,7 @@ package persistence.database.dbConnection.dbTablesEnums;
 public enum UsersTableColumnNameEnums {
     //USER_UUID,USERNAME,USER_PASSWORD,USER_EMAIL,USER_CREATION_DATE,USER_LAST_LOGIN
 
-    USER_UUID("user_id"), USERNAME("username"),USER_PASSWORD("password"),USER_EMAIL("email"),USER_CREATION_DATE(""),USER_LAST_LOGIN("");
+    USER_UUID("user_id"), USERNAME("username"),USER_PASSWORD("password"),USER_EMAIL("email"),USER_CREATION_DATE("date_created"),USER_LAST_LOGIN("last_login");
 
     private final String text;
 
