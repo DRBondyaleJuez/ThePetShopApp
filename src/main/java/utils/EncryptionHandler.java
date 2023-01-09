bf287ab4d28617691dfe9d778444d7eaf81cf5ed
Daniel RBJ <d.r.bond.juez@hotmail.com>
2023-01-09 19:09:26 +0100

    Proper transposition applied during encryption following the Route Cipher as described in wikipedia (Transposition_cipher). There is also a detransposition implemented.

--- src/main/java/utils/EncryptionHandler.java
+++ src/main/java/utils/EncryptionHandler.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class EncryptionHandler {
@@ -11,9 +12,8 @@ public class EncryptionHandler {
     public EncryptionHandler() {
         key = ""+1010110011+0110001011+0111111001;
         saltSize = 500;
-        transpositionValue = 300;
+        transpositionValue = Integer.parseInt(key.substring(0,3));
     }
-
 
     //PROBABLY BOTH METHODS DO THE SAME BUT FOR THE SAKE OF COMPREHENSION THEY ARE GOING TO BE SEPARATED IN TWO METHODS
     public String encrypt(String textToEncrypt){
@@ -42,9 +42,29 @@ public class EncryptionHandler {
 
     private byte[] transposition(byte[] byteArrayToTranspose){
 
+        int lengthOfTranspositionList = byteArrayToTranspose.length/transpositionValue;
+       // int remainderOfTranspositionListLength = byteArrayToTranspose.length % transpositionValue; ------------------------------------------------
+
+        ArrayList<Byte>[] transpositionListMatrix = new ArrayList[transpositionValue+1];
+        for (int i = 0; i < transpositionListMatrix.length; i++) {
+            transpositionListMatrix[i] = new ArrayList<>();
+        }
+        ArrayList<Byte> finalTransposedByteList = new ArrayList<>();
         byte[] transposedByteArray = new byte[byteArrayToTranspose.length];
-        for (int i = 0; i < transposedByteArray.length; i++) {
-            transposedByteArray[(i + transpositionValue) % transposedByteArray.length] = byteArrayToTranspose[i];
+        for (int i = 0; i < byteArrayToTranspose.length; i++) {
+            if(i >= lengthOfTranspositionList * transpositionValue){
+                transpositionListMatrix[transpositionValue].add(byteArrayToTranspose[i]);
+            } else {
+                transpositionListMatrix[i % transpositionValue].add(byteArrayToTranspose[i]);
+            }
+        }
+
+        for (ArrayList<Byte> listMatrix : transpositionListMatrix) {
+            finalTransposedByteList.addAll(listMatrix);
+        }
+
+        for (int i = 0; i < byteArrayToTranspose.length; i++) {
+            transposedByteArray[i] = finalTransposedByteList.get(i);
         }
 
         return transposedByteArray;
@@ -68,12 +88,26 @@ public class EncryptionHandler {
         //Turn decrypted byte array to corresponding string
         return new String(decryptedUnsaltedString);
     }
-    
+
     private byte[] detransposition(byte[] byteArrayToDetranspose){
 
+        int lengthOfTranspositionList = byteArrayToDetranspose.length/transpositionValue;
+
+        ArrayList<Byte> detransposedByteList = new ArrayList<>();
+
         byte[] detransposedByteArray = new byte[byteArrayToDetranspose.length];
-        for (int i = 0; i < detransposedByteArray.length; i++) {
-            detransposedByteArray[i] = byteArrayToDetranspose[(i + transpositionValue) % detransposedByteArray.length];
+
+        for (int i = 0; i < lengthOfTranspositionList; i++) {
+                for (int j = 0; j < transpositionValue; j++) {
+                    detransposedByteList.add(byteArrayToDetranspose[(lengthOfTranspositionList*j) + i]);
+                }
+        }
+        for (int i = lengthOfTranspositionList * transpositionValue; i < byteArrayToDetranspose.length; i++) {
+            detransposedByteList.add(byteArrayToDetranspose[i]);
+        }
+
+        for (int i = 0; i < byteArrayToDetranspose.length; i++) {
+            detransposedByteArray[i] = detransposedByteList.get(i);
         }
 
         return detransposedByteArray;
@@ -81,7 +115,7 @@ public class EncryptionHandler {
     }
 
     private String getSaltString() {
-        String saltChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!$&@_-()[].,*";//1234567890";
+        String saltChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!$&@_-()[].,*";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
         while (salt.length() < saltSize) { // length of the random string.
